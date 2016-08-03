0f778601b8f25d8336cf7c20c04a8597edc68aad
Tihh Gonçalves <tihh.gon@gmail.com>
2016-08-03 15:41:13 -0300

    controle de versão local e remota

--- incs/variables.php
+++ incs/variables.php
@@ -1,6 +1,10 @@
 <?
 
-$url_site = 'http://localhost/github/site_movimento.ielbc.com.br';
+if( $_SERVER['HTTP_HOST'] == 'localhost') {
+  $url_site = 'http://localhost/github/site_movimento.ielbc.com.br';
+} else {
+  $url_site = 'http://movimento.ielbc.com.br';
+}
 
 $midiasocial_facebook = 'http://facebook.com/ielbc';
 
