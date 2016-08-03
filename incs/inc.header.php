95d4f186e67d5b9812835b22b12ce9042ad08f89
Tihh Gonçalves <tihh.gon@gmail.com>
2016-08-03 10:20:43 -0300

    Ajustado em PHP e LESS

---
+++ incs/inc.header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+  <title>Phantom by HTML5 UP</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!--[if lte IE 8]><script src="js/ie/html5shiv.js"></script><![endif]-->
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/less.css" />
+  <!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+  <!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+</head>
+<body>
+<!-- Wrapper -->
+<div id="wrapper">
+
+  <!-- Header -->
+  <header id="header">
+    <div class="inner">
+
+      <!-- Logo -->
+      <a href="index.html" class="logo">
+        <span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Phantom</span>
+      </a>
+
+      <!-- Nav -->
+      <nav>
+        <ul>
+          <li><a href="#menu">Menu</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </header>
+
+  <!-- Menu -->
+  <nav id="menu">
+    <h2>Menu</h2>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="generic.html">Ipsum veroeros</a></li>
+      <li><a href="generic.html">Tempus etiam</a></li>
+      <li><a href="generic.html">Consequat dolor</a></li>
+      <li><a href="elements.html">Elements</a></li>
+    </ul>
+  </nav>
