671d3eae0bbe8f4e6d8e85104c0af50bbdde8961
Tihh Gonçalves <tihh.gon@gmail.com>
2016-08-05 16:01:14 -0300

    Quem Somos

--- quem-somos.php
+++ quem-somos.php
@@ -8,17 +8,17 @@ include('incs/inc.header.php');
     <img src="images/tambor.png" class="image-right">
     <h1>Somos Livres</h1>
     <p>Entendemos que Jesus inaugurou o reino de Deus, ou seja, começou a trazer novamente todo o universo criado para o Criador. Jesus inaugurou a vontade de Deus na terra, como é feita no céu. Somos uma extensão do ministério como homem de Jesus.</p>
-    <p>Queremos ser uma Igreja para quem não gosta de igreja e para pessoas de quem a Igreja não gosta. Um lugar perfeito pra pessoas imperfeitas. Gente feito gente, como a gente. Então acho nossa loucura é só um indício da nossa sanidade espirutual e de que estamos no carminho certo.</p>
+    <p>Queremos ser uma Igreja para quem não gosta de igreja e para pessoas de quem a Igreja não gosta. Um lugar perfeito pra pessoas imperfeitas. Gente feito gente, como a gente. </p>
     <p>Nossa missão consiste em abraçar o ser humano do jeito que ele é, para um dia ser como Deus sonhou. Essa é nossa jornada. Esse é o camino que estamos: o Caminho da Graça. O caminho que é Jesus. E é nessa viagem que a gente segue.</p>
     <p>Relacionamentos. É aí que nasce a Livre. Da ideia que estamos todos juntos e precisamos uns dos outros e que não somos melhores do que ninguém. Somos uma igreja para os fracos, gente que se perdeu na caminhada, gente que nem fazia ideia que existia um caminho. Somos um mendigo que encontrou o pão e nossa missão consiste em mostrar a quem ainda tem fome, onde também encontrá-lo.</p>
     <p>Queremos ser uma expressão histórica do amor fraternal e incondicionao do nosso Deus, que tudo o que quer é estar com a gente. Nossa missão, então, consiste em revelar a todos sua identidade de filho de Deus. O ministério de Jesus estava focado na reconciliação espontânea das pessoas com Deus. Nossa missão é levar o Evangelho todo a todas as pessoas através do amor. Acreditamos que se o amor não mudar uma pessoa, nada mais mudará. Nosso compromisso é com o amor.</p>
 
     <h1>Uma Igreja dentro da igreja</h1>
     <img src="images/mugs.png">
-    <p>Somos uma Igreja dentro da igreja. Louco isso, né? Exatamente. Louco! Mas Paulo já disse que nossa missão nesse mundo pareceria loucura pra muita gente.</p>
-    <p>É que queremos ser mais do que uma instituição, apesar de pertencermos a uma. Entendemos que a vida com Deus vai além das paredes da comunidade e dos encontros de domingo a domingo. Fazemos parte da IELBC (Igreja Evangélica Livre de Balneário Camboriú), mas entendemos que ela, na verdade, é um lugar com pessoas comuns, como um.</p>
-    <p>Não queremos parecer uma igreja, mas somente ser uma. Queremos ser simplesmente simples, assim como Deus é. Acreditamos que ser igreja é a coisa fascinante que alguém poderia experimentar e encontra, nela, tudo o que sempre procurou no lugar errado. Somos um movimento, que visa ir na contramão da superficialidade religiosa. Entendemos que não dá mais pra perder tempo com aquilo que não edifica vidas.</p>
-    <p>Uma Igreja pra quem não gosta de igreja e para pessoas de quem a igreja não gosta. Tipo, você. Participe com a gente nessa loucura!</p>
+    <p>Somos uma Igreja dentro da igreja. Louco isso, né? Exatamente. Louco! Mas Paulo já disse que nossa missão nesse mundo pareceria loucura pra muita gente. Então nossa loucura é só um indício da nossa sanidade espiritual e de que estamos no carminho certo.</p>
+    <p>É que queremos ser mais do que uma instituição, apesar de pertencermos a uma. Entendemos que a vida com Deus vai além das paredes da comunidade e dos encontros de domingo a domingo. Fazemos parte da IELBC (Igreja Evangélica Livre de Balneário Camboriú), mas entendemos que ela, na verdade, é um lugar onde nos encontramos e deixamos de ser uns pra sermos um.</p>
+    <p>Não queremos parecer uma igreja, mas somente ser uma. Queremos ser simplesmente simples, assim como Deus é simples. Acreditamos que ser igreja é a coisa fascinante que alguém poderia experimentar e encontra, nela, tudo o que sempre procurou no lugar errado. Somos um movimento, que visa ir na contramão da superficialidade religiosa. Entendemos que não dá mais pra perder tempo com aquilo que não edifica vidas.</p>
+    <p>Uma Igreja pra quem não gosta de igreja e para pessoas de quem a igreja não gosta. Tipo, você. Participe você também dessa loucura! Seja Livre com a gente.</p>
 
   </div>
 </div>
