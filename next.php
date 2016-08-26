87b34646ed67dd4858a97668f5373da335cf4881
Tiago Gonçalves <Tiago Gonalves>
2016-08-26 15:02:28 -0300

    Next

---
+++ next.php
@@ -0,0 +1,29 @@
+<?
+$subpage = true;
+include('incs/inc.header.php');
+?>
+    <!-- Main -->
+    <div id="main" class="pg_next">
+        <div class="inner">
+
+            <div id="pagina_topo_bg" style="background-image: url(images/next-topo.png);">
+            </div>
+            <p>Tá lançado nossa primeira série no NEXT, chamada VINTAGE.<br>
+                Um convite a viajarmos juntos a era-bíblica, pra entendermos um pouco melhor alguns conceitos básicos da nossa caminhada cristã.</p>
+            <h2>Quando vai rolar?</h2>
+            <p>Não fique de fora! A partir de setembro, todas às sextas, depois das 22hrs.<br>
+                Exceto no segundo final de semana de cada mês, que será ao sábado, depois das 20hrs.
+            </p>
+
+
+            <hr>
+
+            <h2>QUIZ</h2>
+
+            <p>Aproveite a faça perguntas no <a href="<?= $url_site; ?>quiz">nosso Quis</a>. As perguntas selecionadas serão respondidas durante os NEXT’s.</p>
+
+        </div>
+    </div>
+<?
+include('incs/inc.footer.php');
+?>
