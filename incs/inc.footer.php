060eb6d27649c9f0976af069ec890b89b0fe344e
Tihh Gonçalves <tihh.gon@gmail.com>
2016-11-01 16:50:42 -0200

    up

--- incs/inc.footer.php
+++ incs/inc.footer.php
@@ -40,6 +40,17 @@
 
 </script>
 
+
+<!-- Rastreamento smartlook.com -->
+<script type="text/javascript">
+  window.smartlook||(function(d) {
+  var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+  var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+  c.charset='utf-8';c.src='//rec.smartlook.com/recorder.js';h.appendChild(c);
+  })(document);
+  smartlook('init', '0558bde921436c295be10f04b66749032cd546b9');
+</script>
+
 </body>
 </html>
 
