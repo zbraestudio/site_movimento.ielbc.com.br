1c40e37aa67e91b23699f97e55d2e2a371c9e60b
Tihh Gonçalves <tihh.gon@gmail.com>
2016-11-14 16:31:56 -0200

    Correções de texto

--- participe.php
+++ participe.php
@@ -15,8 +15,8 @@ include('incs/inc.header.php');
   <div class="box">
     <span class="image featured"><img src="images/grupo.jpg" alt="" /></span>
     <h3>Estações</h3>
-    <p>Entendemos que vida com Deus é tudo o que temos e principalmente somos. Então não tem a ver com ir a lugares nem mesmo fazer coisas. A bíblia nos conta que Jesus é o único caminho que nos leva a Deus. E é nesse caminho que estamos: o Caminho da Graça. E como em toda caminhada, a gente precisa fazer paradas pra descansar os pés. É onde nasce a nossa Estação, um lugar para descansar, fazer amigos e compartilhar nossas vidas.</p>
-    <p>Cremos que uma Estação não é promovida, mas nasce no coração das pessoas que desejam caminhar juntas, aprendendo mais a respeito de Deus e ajudando umas as outras. Ninguém é obrigado ir a uma Estação, mas ela é feita de pessoas que decidiram fazer parte dela.</p>
+    <p>Entendemos que vida com Deus é tudo o que temos e principalmente somos. Então não tem a ver com ir a lugares nem mesmo fazer coisas. A bíblia nos conta que Jesus é o único caminho que nos leva a Deus. E é nesse caminho que estamos: o Caminho da Graça. E como em toda caminhada, a gente precisa fazer paradas pra descansar os pés. É onde nasce o Next, um lugar para descansar os pés, fazer amigos e compartilhar nossas vidas.</p>
+    <p>Cremos que um Next não é promovido, mas nasce no coração das pessoas que desejam caminhar juntas, não só pra estarem perto uma das outras, mas decidiram dar um passo a mais e fazer de quem está perto seu próximo, aprendendo mais a respeito de Deus e ajudando umas as outras. Ninguém é obrigado ir a um Next, mas ele é feito de pessoas que decidiram fazer parte dele.</p>
     <p>Será um prazer ter você com a gente.</p>
 
   </div>
