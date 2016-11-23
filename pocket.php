f4eb27411acd0bd334b27efd04242f7fd02298fa
Tihh Gonçalves <tihh.gon@gmail.com>
2016-11-23 18:01:56 -0200

    Erro de digitação

--- pocket.php
+++ pocket.php
@@ -17,7 +17,7 @@ include('incs/inc.header.php');
     <h3>Seja LIVRES</h3>
     <p>A palavra <i>pocket</i>, do inglês, significa “<i>de bolso</i>” e nosso desejo é espalhar esses bolsos de avivamento mundo afora. Acreditamos que o chamado da igreja é orgânica e integral e o LIVRES nasce do desejo no coração das pessoas que desejam viver isso intensamente de maneira simples e eficaz, que querem mudar de vida de verdade e mudar a vida das pessoas ao seu redor.</p>
     <p>Um LIVRES Pocket não é promovido, só nasce. Se você nos conheceu e tem o desejo de fazer parte disso, saiba que você é parte de nós.</p>
-    <p>Caso não exista uma Pocket na sua cidade, vamos orar e conversar a respeito de oficializarmos uma. Se você já chegou até aqui, cremos que você já é parte dessa revolução! E juntos, vamos levanto o amor e a graça de Jesus pelos quatro cantos do mundo.</p>
+    <p>Caso não exista uma Pocket na sua cidade, vamos orar e conversar a respeito de oficializarmos uma. Se você já chegou até aqui, cremos que você já é parte dessa revolução! E juntos, vamos levando o amor e a graça de Jesus pelos quatro cantos do mundo.</p>
 
     <h4>Livres Pocket que já existem:</h4>
     <ul>
