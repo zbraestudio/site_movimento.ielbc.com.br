1bfd2611dd407a39b0a7ee6de434cc72be34dda0
root <root@socrates.nbz.net.br>
2016-12-20 21:13:34 -0200

    Muando o nome do DB

--- incs/autoload.php
+++ incs/autoload.php
@@ -1,16 +1,16 @@
 <?
 session_start();
-
 include('config.php');
 include('girafa.db.php');
 include('girafa.tablepost.php');
+
 
 if( $_SERVER['HTTP_HOST'] == 'localhost') {
   $db_host = 'nbz.net.br';
 } else {
   $db_host = 'localhost';
 }
-$db_db      = 'livresweb_com_sistema';
+$db_db      = 'livresweb.com_sistema';
 $dv_user    = 'root';
 $db_pass    = 'nwtiago';
 $db = new nbrDB($db_host, $db_db, $dv_user, $db_pass);
