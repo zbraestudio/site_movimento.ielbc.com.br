a19242cdc2c5061c28c5d8918fc7e38af434daf4
root <root@socrates.nbz.net.br>
2017-02-08 13:57:33 -0200

    Mudando DB

--- incs/autoload.php
+++ incs/autoload.php
@@ -10,7 +10,7 @@ if( $_SERVER['HTTP_HOST'] == 'localhost') {
 } else {
   $db_host = 'localhost';
 }
-$db_db      = 'livresweb.com_sistema';
+$db_db      = 'igrejaestacao.com.br_sistema';
 $dv_user    = 'root';
 $db_pass    = 'nwtiago';
 $db = new nbrDB($db_host, $db_db, $dv_user, $db_pass);
